d78625f5a32b9347ae321c515c5bc157c62c99cd
iuliamed001 <iuliamed001@gmail.com>
2024-02-14 11:41:22 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="card">
+    <center><h1>Valentines Day Card </h1></center>
+    <div class="heart" id="heart1">
+      <div id="half1">
+        <div id="circle"></div>
+        <div id="rec"></div>
+      </div>
+      <div id="half2">
+        <div id="circle"></div>
+        <div id="rec"></div>
+      </div>
     </div>
+    <div id="message">
+      <h2>Happy Valentines Day, Love You</h2>
+    </div>
+    <div class="heart" id="heart2">
+      <div id="half1">
+        <div id="circle"></div>
+        <div id="rec"></div>
+      </div>
+      <div id="half2">
+        <div id="circle"></div>
+        <div id="rec"></div>
+      </div>
+    </div>
+  </div>
   );
 }
 
